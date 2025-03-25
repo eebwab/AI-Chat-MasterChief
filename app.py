63682809321d0da6052054c04b9f1ce4d68fb25a
Rishab Nayak <80428671+eebwab@users.noreply.github.com>
2025-03-24 20:09:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import string
 from flask import Flask, request, render_template
 from google.cloud import dialogflow_v2 as dialogflow
 
-# Set up your Dialogflow API credentials
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\risha\.vscode\Projects\AI Chat-bot\MC-Dialogflow-API-Key.json"
 
 #Initialize the Flask app
@@ -19,7 +19,7 @@ project_id = "heisenbergai-pnyk"
 def generate_random_session_id():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
-# Detect intent function
+
 def detect_intent_texts(user_input, session_id, language_code='en'):
     session = client.session_path(project_id, session_id)
     text_input = dialogflow.TextInput(text=user_input, language_code=language_code)
@@ -37,7 +37,7 @@ def home():
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.form['user_input']
-    session_id = generate_random_session_id()  # Generate random session ID for each interaction
+    session_id = generate_random_session_id()  #Generates random session ID for each interaction
     bot_response = detect_intent_texts(user_input, session_id)
     return render_template('index.html', user_input=user_input, bot_response=bot_response)
 
